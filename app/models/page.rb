87aaa49e271f52d7ed1aac5d5b12e1086ff323c3
gaku1114 <ga1114ku@gmail.com>
2023-02-11 00:29:06 +0900

    バリデーションを設定

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,6 @@
 class Page < ApplicationRecord
+  validates :title,         presence: true
+  validates :url,           presence: true
+  validates :user_id,       presence: true
+  validates :category_id,   presence: true
 end
